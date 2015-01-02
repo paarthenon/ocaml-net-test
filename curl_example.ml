b53ef643041199f6f70d4a9e348c120fb5303a3c
Paarth <paarthk@gmail.com>
2015-01-02 18:54:58 -0500

    nothing major

--- curl_example.ml
+++ curl_example.ml
@@ -1,5 +1,7 @@
 
-  let _ =
-    let connection = Curl.init () in
-      Curl.setopt connection (Curl.CURLOPT_URL Sys.argv.(1));
-      Curl.perform connection
+let open_connection =
+	let connection = Curl.init () in
+	Curl.setopt connection (Curl.CURLOPT_URL Sys.argv.(1));
+	Curl.perform connection
+
+let _ = open_connection
