b4e703df6a750f6b8797d78f243bcb2fedc037e5
Paarth <paarthk@gmail.com>
2015-01-03 18:38:47 -0500

    Attempting to get xml light working to parse output of curl request

--- curl_example.ml
+++ curl_example.ml
@@ -1,7 +1,15 @@
 
+let printfunc input =
+	let xdoc = Xml.parse_string input in
+	print_string (Xml.to_string_fmt xdoc); String.length input
+
+
 let open_connection =
 	let connection = Curl.init () in
 	Curl.setopt connection (Curl.CURLOPT_URL Sys.argv.(1));
+	Curl.setopt connection (Curl.CURLOPT_WRITEFUNCTION printfunc);
 	Curl.perform connection
+	(* let xdoc = Xml.parse_string str in
+	Xml.to_string_fmt xdoc*)
 
 let _ = open_connection
